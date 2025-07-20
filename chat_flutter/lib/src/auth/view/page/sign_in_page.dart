2c14644cdb541595b2dfa3304ba24b73f4aa1d37
Dmitry <76906744+s1r1m1r1@users.noreply.github.com>
2025-07-20 18:35:36 +0200

    update

--- chat_flutter/lib/src/auth/view/page/sign_in_page.dart
+++ chat_flutter/lib/src/auth/view/page/sign_in_page.dart
@@ -1,9 +1,9 @@
 import 'package:chat_flutter/main.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:serverpod_auth_apple_flutter/serverpod_auth_apple_flutter.dart';
 import 'package:serverpod_auth_email_flutter/serverpod_auth_email_flutter.dart';
 import 'package:serverpod_auth_google_flutter/serverpod_auth_google_flutter.dart';
-
 import '../../../../env_keys.dart';
 
 class SignInPage extends StatelessWidget {
@@ -29,7 +29,16 @@ class SignInPage extends StatelessWidget {
                   // clientId: kIsWeb ? null , // Client ID of the client (null on web)
                   serverClientId: kIsWeb ? googleClientId : null, // Client ID from the server (required on web)
                   redirectUri: Uri.parse('http://localhost:8082/googlesignin'),
-                )
+                  onFailure: () {
+                    debugPrint("\n\nFailure GOOGLE\n\n");
+                  },
+                ),
+                SignInWithAppleButton(
+                  onFailure: () {
+                    debugPrint("\n\nFailure APPLE\n\n");
+                  },
+                  caller: client.modules.auth,
+                ),
               ],
             ),
           ),
